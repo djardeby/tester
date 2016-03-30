d9a9feb9f6dda264ebcbc857fea5fc81bef31b59
nrkdja <djardeby@gmail.com>
2016-03-30 09:50:25 +0200

    En första commit

---
+++ tester/src/main/java/a/pair/of/red/socks/App.java
@@ -0,0 +1,13 @@
+package a.pair.of.red.socks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
