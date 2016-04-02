73a3a95a252ce3d65f007a9c52e3a4170ddc9cdc
djardeby <djardeby@gmail.com>
2016-04-02 13:28:42 +0200

    Rensar debug-loggning

--- tester/src/main/java/a/pair/of/red/socks/pieces/Rooks.java
+++ tester/src/main/java/a/pair/of/red/socks/pieces/Rooks.java
@@ -24,9 +24,6 @@ public class Rooks extends Piece {
 		long possibility = tmpBoard & ~(tmpBoard - 1);
 		while (possibility != 0) {
 			int index = Long.numberOfTrailingZeros(possibility);
-
-//			moves.append(index % 8);
-//			moves.append(index / 8);
 			int column =index % 8;
 			int row = 7-(index / 8);
 			moves += boardToMoves(0, lineAttacks((getOwnPieces() | getOtherPieces()), column, row), 0).toString();
@@ -38,25 +35,14 @@ public class Rooks extends Piece {
 
 	protected long lineAttacks(long occ, int column, int row) {
 		long upper = FILE_ATTACK[row][column][UPPER] & occ;
-		logger.debug("upper: {}",upper); // 524296
 		long lower = FILE_ATTACK[row][column][LOWER] & occ;
-		logger.debug("lower: {}",lower); // 578712552117108736
-		long ls1b  = Long.highestOneBit(upper);// & -upper;
-		logger.debug("ls1b: {}",ls1b); // 524288
-		//long orLower = lower | 1;//72057594037927936L;
-		//logger.debug("orLower: {}",orLower);// 650770146155036672
-		long mMs1b = Long.lowestOneBit(lower);// 0x8000000000000000L >> Long.numberOfLeadingZeros (lower | 1);
-		logger.debug("mMs1b: {}",mMs1b); // 2251799813685248
+		long ls1b  = Long.highestOneBit(upper);
+		long mMs1b = Long.lowestOneBit(lower);
 		int numberOfTrailingZeros = Long.numberOfTrailingZeros(ls1b);
-		logger.debug("numberOfTrailingZeros: {}",numberOfTrailingZeros);
 		int numberOfLeadingZeros = Long.numberOfLeadingZeros(mMs1b);
-		logger.debug("numberOfLeadingZeros: {}",numberOfLeadingZeros);
 		long odiff = (0xffffffffffffffffL<< (numberOfTrailingZeros+numberOfLeadingZeros)) >>> numberOfLeadingZeros;
-		logger.debug("odiff: {}",odiff); // 63331869744103424
 		long occupied = FILE_ATTACK[row][column][UPPER] | FILE_ATTACK[row][column][LOWER];
-		logger.debug("occupied: {}",occupied);
 		long returnValue = occupied & odiff & ~getOwnPieces();
-		logger.debug("Returnvalue: {}", returnValue); // 2260596041449472
 		return returnValue;
 	}
 }
