44738f6fc7e8fddc58fcd2254b205d76aa6df3cd
djardeby <djardeby@gmail.com>
2016-04-02 15:43:54 +0200

    Refaktoriserar line attacks

--- tester/src/test/java/a/pair/of/red/socks/pieces/RooksTest.java
+++ tester/src/test/java/a/pair/of/red/socks/pieces/RooksTest.java
@@ -1,21 +1,13 @@
 package a.pair.of.red.socks.pieces;
 
-import a.pair.of.red.socks.board.Colour;
 import a.pair.of.red.socks.board.StandardBoard;
-import a.pair.of.red.socks.utils.Constants;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-
-import static a.pair.of.red.socks.utils.Constants.*;
-import static org.junit.Assert.*;
+import static a.pair.of.red.socks.utils.Constants.LINE_ATTACKS;
 
 public class RooksTest {
 
@@ -50,24 +42,25 @@ public class RooksTest {
 		}*/
 		board.setBlackRookBoard(0x8100000000000081L);//-1L);// 34359738368L);
 		board.setWhitePawnBoard(6389270908594971944L);
-		logger.debug("Board: {}",board.toString());
+		logger.debug("Board: {}", board.toString());
 		//Rooks rooks = new Rooks(Colour.BLACK, 0L,0L,0L);
 		board.setWhiteToMove(false);
 		//board.moves();
-		logger.debug("Moves: {}",board.moves());
+		logger.debug("Moves: {}", board.moves());
 	}
 
 
 	@Test
 	public void arrayTest() {
 		StandardBoard board = new StandardBoard();
-		int square = 7;
-		board.setWhiteKingBoard(LINE_ATTACKS[square][0][0]|LINE_ATTACKS[square][0][1]);
-		board.setBlackKingBoard(LINE_ATTACKS[square][1][1]|LINE_ATTACKS[square][1][0]);
-		logger.debug(board.toString());
-		logger.debug("line_attacks: {}",LINE_ATTACKS.length);
-		logger.debug("line_attacks: {}",LINE_ATTACKS[0].length);
-		logger.debug("line_attacks: {}",LINE_ATTACKS[0][0].length);
+		for (int square = 0; square < LINE_ATTACKS.length; square++) {
+			board.setWhiteKingBoard(LINE_ATTACKS[square][0][0] | LINE_ATTACKS[square][0][1]);
+			board.setBlackKingBoard(LINE_ATTACKS[square][1][1] | LINE_ATTACKS[square][1][0]);
+			logger.debug(board.toString());
+		}
+		logger.debug("line_attacks: {}", LINE_ATTACKS.length);
+		logger.debug("line_attacks: {}", LINE_ATTACKS[0].length);
+		logger.debug("line_attacks: {}", LINE_ATTACKS[0][0].length);
 	}
 
 }
