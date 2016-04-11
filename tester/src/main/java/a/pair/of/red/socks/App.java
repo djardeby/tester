5815ab873a3f206c320d1133196361df55e4198a
nrkdja <djardeby@gmail.com>
2016-04-11 08:43:14 +0200

    Räddar Constants-filen

--- tester/src/main/java/a/pair/of/red/socks/App.java
+++ tester/src/main/java/a/pair/of/red/socks/App.java
@@ -3,11 +3,13 @@ package a.pair.of.red.socks;
 import a.pair.of.red.socks.board.Board;
 import a.pair.of.red.socks.board.BoardType;
 import a.pair.of.red.socks.factory.BoardFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class App
-{
-    public static void main( String[] args )
-    {
+public class App {
+	private static final Logger logger = LoggerFactory.getLogger(App.class);
+    public static void main( String[] args ) {
+		logger.debug("args: {}",args);
 		BoardFactory factory = new BoardFactory();
 		Board board = factory.initBoard(BoardType.StandardBoard);
     }
